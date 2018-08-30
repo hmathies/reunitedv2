d5d4a6d172b0931005dfca392be87c70ef0b303b
Heather <hlmathies@gmail.com>
2018-08-29 20:56:30 -0400

    adding stateless componenets

--- src/features/events/EventForm/EventForm.jsx
+++ src/features/events/EventForm/EventForm.jsx
@@ -1,19 +1,42 @@
 import React, { Component } from "react";
 import { Segment, Form, Button } from "semantic-ui-react";
 
+const emptyEvent = {
+    title: '',
+    date: '',
+    city: '',
+    venue: '',
+    hostedBy: ''
+}
 class EventForm extends Component {
     state = {
-        event: {
-            title: '',
-            date: '',
-            city: '',
-            venue: '',
-            hostedBy: ''
+        event: emptyEvent
+    }
+
+    componentDidMount() {
+        if(this.props.selectedEvent !== null) {
+            this.setState({
+                event: this.props.selectedEvent
+            })
         }
     }
+    componentWillReceiveProps(nextProps) {
+        if(nextProps.selectedEvent !== this.props.selectedEvent) {
+            this.setState({
+                event: nextProps.selectedEvent || emptyEvent
+            })
+        }
+
+    }
+
     onFormSubmit= (evt) => {
         evt.preventDefault();
+        if (this.state.event.id) {
+            this.props.updateEvent(this.state.event);
+        } else {
         this.props.createEvent(this.state.event)
+  
+        }
     }
     onInputChange = (evt) => {
         const newEvent =this.state.event;
@@ -23,7 +46,7 @@ class EventForm extends Component {
         })
     }
     render() {
-        const {handleCancel} = this.props;
+        const {handleCancel, updateEvent} = this.props;
         const {handleCreateEvent} = this.props;
         const {event} = this.state;
         return (
@@ -49,7 +72,7 @@ class EventForm extends Component {
                 <label>Hosted By</label>
                 <input name='hostedBy' value={event.hostedBy} onChange={this.onInputChange}  placeholder="Enter the name of person hosting" />
             </Form.Field>
-            <Button positive type="submit"onClick={handleCreateEvent}>
+            <Button positive type="submit"onClick={handleCreateEvent} updateEvent={updateEvent}>
                 Submit
             </Button>
             <Button onClick={handleCancel} type="button">Cancel</Button>
--- src/features/events/EventList/EventListItem.jsx
+++ src/features/events/EventList/EventListItem.jsx
@@ -4,7 +4,7 @@ import EventListAttendee from './EventListAttendee'
 
 class EventListItem extends Component {
   render() {
-    const {event} = this.props;
+    const {event, onEventOpen} = this.props;
     return (
       <Segment.Group>
         <Segment>
@@ -39,7 +39,7 @@ class EventListItem extends Component {
         </Segment>
         <Segment clearing>
         <span>{event.description}</span>
-          <Button as="a" color="teal" floated="right" content="View" />
+          <Button as="a" color="teal" floated="right" content="View" onClick={onEventOpen(event)}/>
         </Segment>
       </Segment.Group>
     );
