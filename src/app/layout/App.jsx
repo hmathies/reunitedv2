8dfb35c1153615a095d1975bfd3f889e1f765188
Heather <hlmathies@gmail.com>
2018-09-15 17:16:12 -0400

    end of section 8

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -27,12 +27,13 @@ class App extends Component {
               <Container className="main">
                 <Switch>
                   <Route path="/events" component={EventDashboard} />
+                  <Route path="/test" component={TestComponent} />
                   <Route path="/event/:id" component={EventDetailedPage} />
+                  <Route path="/manage/:id" component={EventForm} />
                   <Route path="/people" component={PeopleDashboard} />
                   <Route path="/profile/:id" component={UserDetailedPage} />
                   <Route path="/settings" component={SettingsDashboard} />
                   <Route path="/createEvent" component={EventForm} />
-                  <Route path="/test" component={TestComponent} />
                 </Switch>
               </Container>
             </div>
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './app/layout/App';
 import { configureStore } from './app/store/configureStore';
+import ScrollToTop from './app/common/util/ScrollToTop';
 
 import registerServiceWorker from './registerServiceWorker';
 
@@ -16,9 +17,11 @@ const rootEl = document.getElementById('root');
 let render = () => {
     ReactDOM.render(
         <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+          <BrowserRouter>
+            <ScrollToTop>  
+              <App />
+            </ScrollToTop>
+          </BrowserRouter>
         </Provider>,
         rootEl
     );
